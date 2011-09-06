ea58be3bcbad1384965e139b209871393a6fae07
Christian Smith <smith@anvil.io>
2011-09-06 10:20:19 -0700

    Initialize document join counters when loading the first stream; initialize relation.previousKey to the first element of hashKeys; ensure hashKeys.slice is called with non-negative args.

--- example.js
+++ example.js
@@ -10,19 +10,37 @@ var dzr = require('./lib/index')
 
 // Data sources
 
-function r1 () { return new csv().fromPath('sample/main.csv', { columns: true}); }
+var opts = { columns: true };
+
+function r1 () { 
+  return new csv().fromPath('sample/main.csv', opts).transform(function (data) {
+    if (data.longitude !== null && data.latitude !== null) {
+      data.location = [parseFloat(data.longitude), parseFloat(data.latitude)];
+    }
+    return data;
+  }); 
+}
 r1.key = 'rec_id';
 
-function r2 () { return new csv().fromPath('sample/minerals.csv', { columns: true}); }
+function r2 () { return new csv().fromPath('sample/minerals.csv', opts); }
 r2.foreignKey = 'rec_id';
 r2.property = 'minerals';
 r2.key = 'abbr';
 
-function r3 () { return new csv().fromPath('sample/hostrock.csv', { columns: true}); }
+function r3 () { return new csv().fromPath('sample/hostrock.csv', opts); }
 r3.foreignKey = 'rec_id';
 r3.property = 'hostrock';
 r3.value = 'value';
 
+function r4 () { return new csv().fromPath('sample/reference.csv', opts); }
+r4.foreignKey = 'rec_id';
+r4.property = 'references';
+r4.value = 'reference';
+
+function r5 () { return new csv().fromPath('sample/resource.csv', opts); }
+r5.foreignKey = 'rec_id';
+r5.property = 'resource';
+r5.value = 'value';
 
 // Dnormalizr Usage
 
@@ -38,4 +56,5 @@ dzr.on('data', function (data) {
   });
 });
 
-dzr.join(r1, r2, r3);
+
+dzr.join(r1, r2, r3, r4, r5);
